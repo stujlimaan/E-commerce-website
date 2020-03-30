734d1a2966da0c1e3eadaf2caf061709536091b9
stujlimaan <tujlimaan1999@gmail.com>
2020-03-30 10:07:07 +0530

    .

---
+++ resources/views/backEnd/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="row-fluid">
+    <div id="footer" class="span12">Copyright ©2020 . All rights reserved.</div>
+</div>
---
+++ resources/views/frontEnd/layouts/master.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>@yield('title','Master Page')</title>
+    <link href="{{asset('frontEnd/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('frontEnd/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('frontEnd/css/prettyPhoto.css')}}" rel="stylesheet">
+    <link href="{{asset('frontEnd/css/price-range.css')}}" rel="stylesheet">
+    <link href="{{asset('frontEnd/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('frontEnd/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('frontEnd/css/responsive.css')}}" rel="stylesheet">
+   
+    <link rel="stylesheet" href="{{asset('easyzoom/css/easyzoom.css')}}" />
+</head>
+
+<body>
+@include('frontEnd.layouts.header')
+@section('slider')
+    @include('frontEnd.layouts.slider')
+@show
+@yield('content')
+@include('frontEnd.layouts.footer')
+<script src="{{asset('frontEnd/js/jquery.js')}}"></script>
+<script src="{{asset('frontEnd/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('frontEnd/js/jquery.scrollUp.min.js')}}"></script>
+<script src="{{asset('frontEnd/js/price-range.js')}}"></script>
+<script src="{{asset('frontEnd/js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('frontEnd/js/main.js')}}"></script>
+<script src="{{asset('easyzoom/dist/easyzoom.js')}}"></script>
+<script>
+    
+    var $easyzoom = $('.easyzoom').easyZoom();
+
+    
+    var api1 = $easyzoom.filter('.easyzoom--with-thumbnails').data('easyZoom');
+
+    $('.thumbnails').on('click', 'a', function(e) {
+        var $this = $(this);
+
+        e.preventDefault();
+
+       
+        api1.swap($this.data('standard'), $this.attr('href'));
+    });
+
+    
+    var api2 = $easyzoom.filter('.easyzoom--with-toggle').data('easyZoom');
+
+    $('.toggle').on('click', function() {
+        var $this = $(this);
+
+        if ($this.data("active") === true) {
+            $this.text("Switch on").data("active", false);
+            api2.teardown();
+        } else {
+            $this.text("Switch off").data("active", true);
+            api2._init();
+        }
+    });
+</script>
+</body>
+</html>
